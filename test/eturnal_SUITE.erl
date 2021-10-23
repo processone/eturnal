6bb456b53b99fcc83b662fedad84243bbc63d179
Holger Weiss <holger@zedat.fu-berlin.de>
2021-10-23 13:31:32 +0200

    Add function specifications to test suite

--- test/eturnal_SUITE.erl
+++ test/eturnal_SUITE.erl
@@ -20,23 +20,36 @@
 -compile(export_all).
 -include_lib("common_test/include/ct.hrl").
 
+-type info() :: ct_suite:ct_info().
+-type config() :: ct_suite:ct_config().
+-type test_def() :: ct_suite:ct_test_def().
+-type test_name() :: ct_suite:ct_testname().
+-type group_def() :: ct_suite:ct_group_def().
+-type group_name() :: ct_suite:ct_groupname().
+
 %% API.
 
+-spec suite() -> [info()].
 suite() ->
     [{timetrap, {seconds, 30}}].
 
+-spec init_per_suite(config()) -> config().
 init_per_suite(Config) ->
     Config.
 
+-spec end_per_suite(config()) -> ok.
 end_per_suite(_Config) ->
     ok.
 
+-spec init_per_group(group_name(), config()) -> config().
 init_per_group(_GroupName, Config) ->
     Config.
 
+-spec end_per_group(group_name(), config()) -> ok.
 end_per_group(_GroupName, _Config) ->
     ok.
 
+-spec init_per_testcase(test_name(), config()) -> config().
 init_per_testcase(start_eturnal, Config) ->
     DataDir = ?config(data_dir, Config),
     ConfFile = filename:join(DataDir, "eturnal.yml"),
@@ -47,12 +60,15 @@ init_per_testcase(start_eturnal, Config) ->
 init_per_testcase(_TestCase, Config) ->
     Config.
 
+-spec end_per_testcase(test_name(), config()) -> ok.
 end_per_testcase(_TestCase, _Config) ->
     ok.
 
+-spec groups() -> [group_def()].
 groups() ->
     [].
 
+-spec all() -> [test_def()] | {skip, term()}.
 all() ->
     [start_eturnal,
      check_info,
@@ -64,39 +80,48 @@ all() ->
      connect_tls,
      stop_eturnal].
 
+-spec start_eturnal(config()) -> any().
 start_eturnal(_Config) ->
     {ok, _} = application:ensure_all_started(eturnal).
 
+-spec check_info(config()) -> any().
 check_info(_Config) ->
     {ok, Info} = eturnal_ctl:get_info(),
     true = is_list(Info).
 
+-spec check_sessions(config()) -> any().
 check_sessions(_Config) ->
     {ok, Sessions} = eturnal_ctl:get_version(),
     true = is_list(Sessions).
 
+-spec check_loglevel(config()) -> any().
 check_loglevel(_Config) ->
     {ok, Level} = eturnal_ctl:get_loglevel(),
     true = Level =:= "debug".
 
+-spec check_version(config()) -> any().
 check_version(_Config) ->
     {ok, Version} = eturnal_ctl:get_version(),
     match = re:run(Version, "^[0-9]+\\.[0-9]+\\.[0-9](\\+[0-9]+)?",
                    [{capture, none}]).
 
+-spec reload(config()) -> any().
 reload(_Config) ->
     ok = eturnal_ctl:reload().
 
+-spec connect_tcp(config()) -> any().
 connect_tcp(_Config) ->
     {ok, Addr} = inet:parse_address("127.0.0.1"),
     {ok, Sock} = gen_tcp:connect(Addr, 34780, []),
     ok = gen_tcp:close(Sock).
 
+-spec connect_tls(config()) -> any().
 connect_tls(_Config) ->
     {ok, Addr} = inet:parse_address("127.0.0.1"),
     {ok, TCPSock} = gen_tcp:connect(Addr, 53490, []),
     {ok, TLSSock} = fast_tls:tcp_to_tls(TCPSock, [connect]),
     ok = fast_tls:close(TLSSock).
 
+-spec stop_eturnal(config()) -> any().
 stop_eturnal(_Config) ->
     ok = application:stop(eturnal).
