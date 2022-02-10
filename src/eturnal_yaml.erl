processone/eturnal

commit 333b66799f70073c42bd6b0a7a31ade855ef1512

Support HAproxy protocol (v1 and v2)
If the new 'listen' option 'proxy_protocol' is set to 'true', the client
IP address/port is retrieved from the HAproxy protocol header.

Closes #18.

src/eturnal_yaml.erl
@@ -26,7 +26,7 @@
                options/1, options/2, port/0, pos_int/1]).
 
 -type listener() :: {inet:ip_address(), inet:port_number(), eturnal:transport(),
-                     boolean()}.
+                     boolean(), boolean()}.
 
 -define(BLACKLIST, [{{127, 0, 0, 0}, 8},               % IPv4 loopback.
                     {{0, 0, 0, 0, 0, 0, 0, 1}, 128}]). % IPv6 loopback.
@@ -112,6 +112,7 @@ listen_validator() ->
             #{ip => ip(),
               port => int(0, 65535),
               transport => enum([tcp, udp, tls, auto]),
+              proxy_protocol => bool(),
               enable_turn => bool()},
             [unique,
              {required, [ip]}]),
@@ -124,8 +125,9 @@ listen_validator() ->
                   I = proplists:get_value(ip, Opts),
                   T = proplists:get_value(transport, Opts, udp),
                   P = proplists:get_value(port, Opts, DefP(T)),
+                  X = proplists:get_value(proxy_protocol, Opts, false),
                   E = proplists:get_value(enable_turn, Opts, true),
-                  {I, P, T, E}
+                  {I, P, T, X, E}
           end)),
       fun check_overlapping_listeners/1).
 
@@ -140,7 +142,8 @@ check_overlapping_listeners(Listeners) ->
       -> ok | no_return().
 check_overlapping_listeners(Listeners, PrepareFun) ->
     _ = lists:foldl(
-          fun({IP, Port, Transport, _EnableTURN} = Listener, Acc) ->
+          fun({IP, Port, Transport, _ProxyProtocol, _EnableTURN} = Listener,
+              Acc) ->
                   Key = case Transport of
                             udp ->
                                 {IP, Port, udp};
@@ -171,7 +174,7 @@ check_overlapping_listeners(Listeners, PrepareFun) ->
     ok.
 
 -spec format_listener(listener()) -> binary().
-format_listener({IP, Port, Transport, _EnableTURN}) ->
+format_listener({IP, Port, Transport, _ProxyProtocol, _EnableTURN}) ->
     Addr = eturnal_misc:addr_to_str(IP, Port),
     list_to_binary(io_lib:format("~s (~s)", [Addr, Transport])).
 

doc/overview.edoc
@@ -133,7 +133,8 @@ listens on. Each `listen' entry holds one or more of the following key-value
 pairs:
 
 <ul>
-  <li><a name="ip" class="cue">`ip:' <em>address</em></a><br/>
+  <li><a name="ip"
+         class="cue">`ip:' <em>address</em></a><br/>
       The `ip' <em>address</em> must be a string denoting the IPv4 or IPv6
       address (note that IPv6 addresses with leading `"::"' must be enclosed
       with quotation marks). This is the only mandatory key-value pair for each
@@ -141,15 +142,24 @@ pairs:
       IPv4 interfaces. Specifying `"::"' tells eturnal to listen on all IPv6
       interfaces, and possibly also all IPv4 interfaces, depending on the
       operating system (configuration).</li>
-  <li><a name="port" class="cue">`port:' <em>number</em></a><br/>
+  <li><a name="port"
+         class="cue">`port:' <em>number</em></a><br/>
       The (optional) `port' <em>number</em> must be between `0' and `65535',
       and is usually between `1024' and `49151'. The default value is `5349'
       if the {@section transport} is set to `tls', otherwise `3478'.</li>
-  <li><a name="transport" class="cue">`transport:' <em>protocol</em></a><br/>
+  <li><a name="transport"
+         class="cue">`transport:' <em>protocol</em></a><br/>
       The (optional) `transport' <em>protocol</em> must be one of `udp', `tcp',
       `tls', or, in order to accept unencrypted TCP and TLS connections on the
       same port, `auto'. The default value is `udp'.</li>
-  <li><a name="enable_turn" class="cue">`enable_turn:' <em>flag</em></a><br/>
+  <li><a name="proxy_protocol"
+         class="cue">`proxy_protocol:' <em>flag</em></a><br/>
+      If the (optional) `proxy_protocol' <em>flag</em> is set to `true', the
+      client IP address/port is retrieved from the HAproxy protocol header. This
+      is not supported if the `transport' is `udp'. The default value is
+      `false'.</li>
+  <li><a name="enable_turn"
+         class="cue">`enable_turn:' <em>flag</em></a><br/>
       The (optional) `enable_turn' <em>flag</em> must be `true' to enable TURN
       in addition to STUN, or `false' to offer just STUN on this socket. The
       default value is `true'.</li>

rebar.config
@@ -2,7 +2,7 @@
 
 {deps,
  [{conf, "~> 0.1"},
-  {stun, "~> 1.0.46"},
+  {stun, {git, "https://github.com/processone/stun.git", {branch, "master"}}},
   {influx_udp, "~> 1.1.1"},
   recon]}.
 

src/eturnal.erl
@@ -1,4 +1,4 @@
-%%% eturnal STUN/TURN server.
+%%% eturnal stun/turn server.
 %%%
 %%% Copyright (c) 2020, 2021 Holger Weiss <holger@zedat.fu-berlin.de>.
 %%% Copyright (c) 2020, 2021 ProcessOne, SARL.
@@ -114,17 +114,23 @@ abort(Reason) ->
 -spec init(any()) -> {ok, state()} | no_return().
 init(_Opts) ->
     process_flag(trap_exit, true),
+    case turn_enabled() of
+        true ->
+            check_turn_config(got_secret(), got_relay_addr());
+        false ->
+            ?LOG_DEBUG("TURN is disabled")
+    end,
     case ensure_run_dir() of
         ok ->
             ok;
         error -> % Has been logged.
             abort(run_dir_failure)
     end,
-    case turn_enabled() of
-        true ->
-            check_turn_config(got_secret(), got_relay_addr());
-        false ->
-            ?LOG_DEBUG("TURN is disabled")
+    case check_proxy_config() of
+        ok ->
+            ok;
+        error -> % Has been logged.
+            abort(proxy_config_failure)
     end,
     case check_pem_file() of
         Result when Result =:= ok;
@@ -279,13 +285,14 @@ start_listeners() ->
              end, opt_map()) ++ [{auth_fun, fun ?MODULE:get_password/2},
                                  {hook_fun, fun ?MODULE:run_hook/2}],
     try lists:map(
-          fun({IP, Port, Transport, EnableTURN}) ->
+          fun({IP, Port, Transport, ProxyProtocol, EnableTURN}) ->
                   Opts1 = tls_opts(Transport) ++ Opts,
                   Opts2 = turn_opts(EnableTURN) ++ Opts1,
+                  Opts3 = proxy_opts(ProxyProtocol) ++ Opts2,
                   ?LOG_DEBUG("Starting listener ~s (~s) with options:~n~p",
                              [eturnal_misc:addr_to_str(IP, Port),
-                              Transport, Opts2]),
-                  case stun_listener:add_listener(IP, Port, Transport, Opts2) of
+                              Transport, Opts3]),
+                  case stun_listener:add_listener(IP, Port, Transport, Opts3) of
                       ok ->
                           ?LOG_INFO("Listening on ~s (~s) (~s)",
                                     [eturnal_misc:addr_to_str(IP, Port),
@@ -356,15 +363,19 @@ turn_opts(EnableTURN) ->
              {auth_type, anonymous}]
     end.
 
+-spec proxy_opts(boolean()) -> proplists:proplist().
+proxy_opts(true = _ProxyProtocol) -> [proxy_protocol];
+proxy_opts(false = _ProxyProtocol) -> [].
+
 -spec tls_enabled() -> boolean().
 tls_enabled() ->
-    lists:any(fun({_IP, _Port, Transport, _EnableTURN}) ->
+    lists:any(fun({_IP, _Port, Transport, _ProxyProtocol, _EnableTURN}) ->
                       (Transport =:= tls) or (Transport =:= auto)
               end, get_opt(listen)).
 
 -spec turn_enabled() -> boolean().
 turn_enabled() ->
-    lists:any(fun({_IP, _Port, _Transport, EnableTURN}) ->
+    lists:any(fun({_IP, _Port, _Transport, _ProxyProtocol, EnableTURN}) ->
                       EnableTURN =:= true
               end, get_opt(listen)).
 
@@ -594,6 +605,19 @@ check_turn_config(_GotSecret = true, _GotAddr = false) ->
     ?LOG_WARNING("Specify a 'relay_ipv4_addr' to enable TURN");
 check_turn_config(_GotSecret = false, _GotAddr = false) ->
     ?LOG_WARNING("Specify a 'secret' and 'relay_ipv4_addr' to enable TURN").
+
+-spec check_proxy_config() -> ok | error.
+check_proxy_config() ->
+    case lists:any(
+           fun({_IP, _Port, Transport, ProxyProtocol, _EnableTURN}) ->
+                   (Transport =:= udp) and (ProxyProtocol =:= true)
+           end, get_opt(listen)) of
+        true ->
+            ?LOG_CRITICAL("The 'proxy_protocol' ist not supported 'udp'"),
+            error;
+        false ->
+            ok
+    end.
 
 -spec describe_listener(boolean()) -> binary().
 describe_listener(_EnableTURN = true) ->

commit 27095d24779603f97addd9da988b4f8bdb30d999

Don't crash without 'listen' configuration
Fix the problem that eturnal failed to start up without explicit
'listen' configuration.  This regression was introduced by commit
333b66799f70073c42bd6b0a7a31ade855ef1512.

src/eturnal_yaml.erl
@@ -67,8 +67,8 @@ validator() ->
       [unique,
        {required, []},
        {defaults,
-        #{listen => [{{0, 0, 0, 0, 0, 0, 0, 0}, 3478, udp, true},
-                     {{0, 0, 0, 0, 0, 0, 0, 0}, 3478, tcp, true}],
+        #{listen => [{{0, 0, 0, 0, 0, 0, 0, 0}, 3478, udp, false, true},
+                     {{0, 0, 0, 0, 0, 0, 0, 0}, 3478, tcp, false, true}],
           relay_ipv4_addr => eturnal_misc:my_ipv4_addr(),
           relay_ipv6_addr => eturnal_misc:my_ipv6_addr(),
           relay_min_port => 49152,