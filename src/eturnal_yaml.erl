processone/eturnal

commit 4c8c7afb7f23f3cc80ad5a15ceed17ef879b1238

Use rand:bytes/1 on Erlang/OTP 24 and later
Use rand:bytes/1 for generating the pseudorandom default 'secret' on
Erlang/OTP 24 and later.

src/eturnal_yaml.erl
@@ -204,8 +204,13 @@ get_default(Var, Default) ->
     end.
 
 -spec make_random_secret() -> binary().
+-ifdef(old_rand).
 make_random_secret() ->
     <<(rand:uniform(16#80000000000000000000000000000000)):128>>.
+-else.
+make_random_secret() ->
+    rand:bytes(16).
+-endif.
 
 -spec openssl_list(char()) -> fun((binary() | [binary()]) -> binary()).
 openssl_list(Sep) ->

rebar.config
@@ -59,6 +59,7 @@
 {erl_opts,
  [{platform_define, "^21\.[0-2]\.", old_persistent_term},
   {platform_define, "^21\.[0-2]\.", old_logger},
+  {platform_define, "^2[123]\.", old_rand},
   {platform_define, "^2[12]\.", old_crypto},
   {platform_define, "^2[12]\.", old_inet_backend},
   {platform_define, "win32", old_inet_backend},

commit a5f55d1d0f97c3c17bae3614520aa1f39dd54f39

eturnal_yaml: Don't crash without 'secret'
If no 'secret' is configured in the eturnal.yml file, eturnal falls back
to the environment variable ETURNAL_SECRET, and otherwise to a random
secret.  Use the correct data type for those fallback values in order to
avoid crashes.

This fixes a regression introduced by commit
4c8c7afb7f23f3cc80ad5a15ceed17ef879b1238.

Closes #21.

src/eturnal_yaml.erl
@@ -85,7 +85,7 @@ validator() ->
           whitelist => [],
           strict_expiry => false,
           realm => <<"eturnal.net">>,
-          secret => get_default(secret, make_random_secret()),
+          secret => [get_default(secret, make_random_secret())],
           software_name => <<"eturnal">>,
           run_dir => get_default("RUNTIME_DIRECTORY", <<"run">>),
           log_dir => get_default("LOGS_DIRECTORY", <<"log">>),