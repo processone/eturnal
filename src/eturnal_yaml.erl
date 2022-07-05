processone/eturnal

commit a19bb7c88f26af55406b8c2fab6844d9bd183931

Support environment variables for relay addresses
Fall back to reading environment variables when 'relay_ipv4_address'
and/or 'relay_ipv6_address' aren't specified.  This may be convenient
esp. for Docker users.

Closes #24.

src/eturnal_yaml.erl
@@ -69,8 +69,10 @@ validator() ->
        {defaults,
         #{listen => [{{0, 0, 0, 0, 0, 0, 0, 0}, 3478, udp, false, true},
                      {{0, 0, 0, 0, 0, 0, 0, 0}, 3478, tcp, false, true}],
-          relay_ipv4_addr => eturnal_misc:my_ipv4_addr(),
-          relay_ipv6_addr => eturnal_misc:my_ipv6_addr(),
+          relay_ipv4_addr => get_default(relay_ipv4_addr,
+                                         eturnal_misc:my_ipv4_addr()),
+          relay_ipv6_addr => get_default(relay_ipv6_addr,
+                                         eturnal_misc:my_ipv6_addr(),
           relay_min_port => 49152,
           relay_max_port => 65535,
           tls_crt_file => none,

doc/overview.edoc
@@ -297,9 +297,10 @@ Default: Autodetected IPv4 address (or `none')</div>
 This option specifies the IPv4 address used for relaying data from/to TURN
 peers. Note that this <em>must</em> be the server's public IPv4 address; i.e.,
 it cannot be set to, for example, `"0.0.0.0"'. If this option isn't specified,
-eturnal will try to autodetect the system's IPv4 address. If `relay_ipv4_addr'
-is unspecified and the host name doesn't resolve to a public IPv4 address,
-eturnal won't offer TURN relaying from/to IPv4 peers.
+the value of the envionment variable {@section ETURNAL_RELAY_IPV4_ADDR} is used
+instead. If this environment variable is unset as well, eturnal will try to
+autodetect the system's IPv4 address. If this fails, eturnal won't offer TURN
+relaying from/to IPv4 peers.
 
 === relay_ipv6_addr ===
 
@@ -309,9 +310,10 @@ Default: Autodetected IPv6 address (or `none')</div>
 This option specifies the IPv6 address used for relaying data from/to TURN
 peers. Note that this <em>must</em> be the server's public IPv6 address; i.e.,
 it cannot be set to, for example, `"::"'. If this option isn't specified,
-eturnal will try to autodetect the system's IPv6 address. If `relay_ipv6_addr'
-is unspecified and the host name doesn't resolve to a public IPv6 address,
-eturnal won't offer TURN relaying from/to IPv6 peers.
+the value of the envionment variable {@section ETURNAL_RELAY_IPV6_ADDR} is used
+instead. If this environment variable is unset as well, eturnal will try to
+autodetect the system's IPv6 address. If this fails, eturnal won't offer TURN
+relaying from/to IPv6 peers.
 
 === relay_max_port ===
 

commit bdd95f4f3923878023364d6a2edecdba9f7e0a65

eturnal_yaml: Fix syntax error
Fix a syntax error introduced by commit
a19bb7c88f26af55406b8c2fab6844d9bd183931.

src/eturnal_yaml.erl
@@ -72,7 +72,7 @@ validator() ->
           relay_ipv4_addr => get_default(relay_ipv4_addr,
                                          eturnal_misc:my_ipv4_addr()),
           relay_ipv6_addr => get_default(relay_ipv6_addr,
-                                         eturnal_misc:my_ipv6_addr(),
+                                         eturnal_misc:my_ipv6_addr()),
           relay_min_port => 49152,
           relay_max_port => 65535,
           tls_crt_file => none,