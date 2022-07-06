30e6bafa7f27d468f56863f478e8bde54ae46fb2
Holger Weiss <holger@zedat.fu-berlin.de>
2022-07-06 21:31:46 +0200

    Let windows/make-installer set version string
    Make sure development version strings (such as 1.0.0+42) are also used
    by Inno Setup.  This is required for setting the correct path to the
    sys.config file, for example.

--- windows/delay-start.cmd
+++ windows/delay-start.cmd
@@ -1,6 +1,6 @@
 @echo off
 set rel_name="eturnal"
-set rel_vsn="1.8.3"
+set rel_vsn="@VERSION@"
 setlocal enabledelayedexpansion
 for /f "tokens=3" %%i in ('sc getkeyname %rel_name%_%rel_vsn%') do (
     set svc_name=%%i
